4527f7fe4325eee9d44704bf62377581e8b662ba
Troy Benson <troybensonsa@gmail.com>
2024-12-03 05:29:07 +0000

    day 3

--- days/day3/src/part1.rs
+++ days/day3/src/part1.rs
@@ -1,6 +1,64 @@
 use utils::*;
 
+macro_rules! parse_char {
+    ($input:expr, $char:literal) => {{
+        if $input.peek() != Some(&$char) {
+            continue;
+        }
+
+        $input.next();
+    }};
+}
+
+macro_rules! parse_number {
+    ($input:expr) => {{
+        let mut number = None;
+        
+        for _ in 0..3 {
+            let Some(next) = $input.peek() else {
+                break;
+            };
+
+            if next.is_digit(10) {
+                number = Some(number.unwrap_or(0u32) * 10 + next.to_digit(10).unwrap());
+                $input.next();
+            } else {
+                break;
+            }
+        }
+
+        number
+    }};
+}
+
 fn main() {
     let input = init!();
-    todo!("{}", input);
+
+    let mut sum = 0;
+
+    let mut input = input.chars().peekable();
+
+    while let Some(next) = input.next() {
+        if next == 'm' {
+            parse_char!(input, 'u');
+            parse_char!(input, 'l');
+            parse_char!(input, '(');
+
+            let Some(first) = parse_number!(input) else {
+                continue;
+            };
+
+            parse_char!(input, ',');
+
+            let Some(second) = parse_number!(input) else {
+                continue;
+            };
+
+            parse_char!(input, ')');
+
+            sum += first * second;
+        }
+    }
+
+    println!("{}", sum);
 }
--- days/day3/src/part2.rs
+++ days/day3/src/part2.rs
@@ -1,6 +1,88 @@
 use utils::*;
 
+macro_rules! parse_char {
+    ($input:expr, $char:literal) => {{
+        if $input.peek() != Some(&$char) {
+            continue;
+        }
+
+        $input.next();
+    }};
+}
+
+macro_rules! parse_number {
+    ($input:expr) => {{
+        let mut number = None;
+        
+        for _ in 0..3 {
+            let Some(next) = $input.peek() else {
+                break;
+            };
+
+            if next.is_digit(10) {
+                number = Some(number.unwrap_or(0u32) * 10 + next.to_digit(10).unwrap());
+                $input.next();
+            } else {
+                break;
+            }
+        }
+
+        number
+    }};
+}
+
 fn main() {
     let input = init!();
-    todo!("{}", input);
+
+    let mut sum = 0;
+
+    let mut enabled = true;
+
+    let mut input = input.chars().peekable();
+
+    while let Some(next) = input.next() {
+        match next {
+            'm' if enabled => {
+                parse_char!(input, 'u');
+                parse_char!(input, 'l');
+                parse_char!(input, '(');
+
+                let Some(first) = parse_number!(input) else {
+                    continue;
+                };
+
+                parse_char!(input, ',');
+
+                let Some(second) = parse_number!(input) else {
+                    continue;
+                };
+
+                parse_char!(input, ')');
+
+                sum += first * second;
+            }
+            'd' => {
+                parse_char!(input, 'o');
+                match input.peek() {
+                    Some(&'n') => {
+                        parse_char!(input, 'n');
+                        parse_char!(input, '\'');
+                        parse_char!(input, 't');
+                        parse_char!(input, '(');
+                        parse_char!(input, ')');
+                        enabled = false;
+                    }
+                    Some(&'(') => {
+                        parse_char!(input, '(');
+                        parse_char!(input, ')');
+                        enabled = true;
+                    }
+                    _ => {},
+                }
+            }
+            _ => {},
+        }
+    }
+
+    println!("{}", sum);
 }
