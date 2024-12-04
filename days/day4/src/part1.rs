c0e7cb3c6c0eebd9944bdd5b425c7eab25afcea3
Troy Benson <troybensonsa@gmail.com>
2024-12-04 05:47:52 +0000

    better day4p1

--- days/day4/src/part1.rs
+++ days/day4/src/part1.rs
@@ -1,6 +1,31 @@
 use itertools::Itertools;
 use utils::*;
 
+const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
+
+fn check_xmas(input: &Vec<Vec<char>>, y: usize, x: usize, dx: i32, dy: i32) -> bool {
+    let height = input.len();
+    let width = input[0].len();
+
+    let max_y = y as i32 + dy * (XMAS.len() - 1) as i32;
+    let max_x = x as i32 + dx * (XMAS.len() - 1) as i32;
+
+    if max_y < 0 || max_x < 0 || max_y >= height as i32 || max_x >= width as i32 {
+        return false;
+    }
+
+    for i in 0..XMAS.len() {
+        let y = y as i32 + i as i32 * dy;
+        let x = x as i32 + i as i32 * dx;
+
+        if input[y as usize][x as usize] != XMAS[i] {
+            return false;
+        }
+    }
+
+    true
+}
+
 fn main() {
     let input = init!();
 
@@ -8,183 +33,30 @@ fn main() {
 
     let input = input
         .lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-    let height = input.len();
-    let width = input[0].len();
-
-    let mut xmas_positions = input
-        .iter()
-        .map(|line| line.iter().map(|_| false).collect_vec())
+        .map(|line| line.chars().collect_vec())
         .collect_vec();
 
     for (y, line) in input.iter().enumerate() {
         for (x, c) in line.iter().enumerate() {
             if *c == 'X' {
-                // Check all 8 directions for XMAS
-                const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
+                let directions = [
+                    (0, 1),
+                    (-1, 1),
+                    (1, 1),
+                    (0, -1),
+                    (-1, -1),
+                    (1, -1),
+                    (1, 0),
+                    (-1, 0),
+                ];
 
-                let check_down = y + XMAS.len() <= height;
-                let check_up = y >= XMAS.len() - 1;
-                let check_right = x + XMAS.len() <= width;
-                let check_left = x >= XMAS.len() - 1;
-
-                if check_down {
-                    // Check down
-                    let mut check = true;
-                    for i in 0..XMAS.len() {
-                        if input[y + i][x] != XMAS[i] {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    if check {
+                for (dx, dy) in directions {
+                    if check_xmas(&input, y, x, dx, dy) {
                         sum += 1;
-                        for i in 0..XMAS.len() {
-                            xmas_positions[y + i][x] = true;
-                        }
-                    }
-
-                    if check_left {
-                        // Check down-left
-                        let mut check = true;
-                        for i in 0..XMAS.len() {
-                            if input[y + i][x - i] != XMAS[i] {
-                                check = false;
-                                break;
-                            }
-                        }
-
-                        if check {
-                            sum += 1;
-                            for i in 0..XMAS.len() {
-                                xmas_positions[y + i][x - i] = true;
-                            }
-                        }
-                    }
-
-                    if check_right {
-                        // Check down-right
-                        let mut check = true;
-                        for i in 0..XMAS.len() {
-                            if input[y + i][x + i] != XMAS[i] {
-                                check = false;
-                                break;
-                            }
-                        }
-
-                        if check {
-                            sum += 1;
-                            for i in 0..XMAS.len() {
-                                xmas_positions[y + i][x + i] = true;
-                            }
-                        }
-                    }
-                }
-
-                if check_up {
-                    // Check up
-                    let mut check = true;
-                    for i in 0..XMAS.len() {
-                        if input[y - i][x] != XMAS[i] {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    if check {
-                        sum += 1;
-                        for i in 0..XMAS.len() {
-                            xmas_positions[y - i][x] = true;
-                        }
-                    }
-
-                    if check_left {
-                        // Check up-left
-                        let mut check = true;
-                        for i in 0..XMAS.len() {
-                            if input[y - i][x - i] != XMAS[i] {
-                                check = false;
-                                break;
-                            }
-                        }
-
-                        if check {
-                            sum += 1;
-                            for i in 0..XMAS.len() {
-                                xmas_positions[y - i][x - i] = true;
-                            }
-                        }
-                    }
-
-                    if check_right {
-                        // Check up-right
-                        let mut check = true;
-                        for i in 0..XMAS.len() {
-                            if input[y - i][x + i] != XMAS[i] {
-                                check = false;
-                                break;
-                            }
-                        }
-
-                        if check {
-                            sum += 1;
-                            for i in 0..XMAS.len() {
-                                xmas_positions[y - i][x + i] = true;
-                            }
-                        }
-                    }
-                }
-
-                if check_right {
-                    // Check right
-                    let mut check = true;
-                    for i in 0..XMAS.len() {
-                        if input[y][x + i] != XMAS[i] {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    if check {
-                        sum += 1;
-                        for i in 0..XMAS.len() {
-                            xmas_positions[y][x + i] = true;
-                        }
-                    }
-                }
-
-                if check_left {
-                    // Check left
-                    let mut check = true;
-                    for i in 0..XMAS.len() {
-                        if input[y][x - i] != XMAS[i] {
-                            check = false;
-                            break;
-                        }
-                    }
-
-                    if check {
-                        sum += 1;
-                        for i in 0..XMAS.len() {
-                            xmas_positions[y][x - i] = true;
-                        }
                     }
                 }
             }
         }
-    }
-
-    for (y, line) in xmas_positions.iter().enumerate() {
-        println!(
-            "{}",
-            line.iter()
-                .enumerate()
-                .map(|(x, c)| if *c { input[y][x] } else { '.' })
-                .collect::<String>()
-        );
     }
 
     println!("{}", sum);
