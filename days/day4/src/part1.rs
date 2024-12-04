eb601baf0366c663f25ed3c332eca5f56b9e0f16
Troy Benson <troybensonsa@gmail.com>
2024-12-04 05:29:31 +0000

    day 4

--- days/day4/src/part1.rs
+++ days/day4/src/part1.rs
@@ -1,6 +1,191 @@
+use itertools::Itertools;
 use utils::*;
 
 fn main() {
     let input = init!();
-    todo!("{}", input);
+
+    let mut sum = 0;
+
+    let input = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let height = input.len();
+    let width = input[0].len();
+
+    let mut xmas_positions = input
+        .iter()
+        .map(|line| line.iter().map(|_| false).collect_vec())
+        .collect_vec();
+
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'X' {
+                // Check all 8 directions for XMAS
+                const XMAS: [char; 4] = ['X', 'M', 'A', 'S'];
+
+                let check_down = y + XMAS.len() <= height;
+                let check_up = y >= XMAS.len() - 1;
+                let check_right = x + XMAS.len() <= width;
+                let check_left = x >= XMAS.len() - 1;
+
+                if check_down {
+                    // Check down
+                    let mut check = true;
+                    for i in 0..XMAS.len() {
+                        if input[y + i][x] != XMAS[i] {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if check {
+                        sum += 1;
+                        for i in 0..XMAS.len() {
+                            xmas_positions[y + i][x] = true;
+                        }
+                    }
+
+                    if check_left {
+                        // Check down-left
+                        let mut check = true;
+                        for i in 0..XMAS.len() {
+                            if input[y + i][x - i] != XMAS[i] {
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if check {
+                            sum += 1;
+                            for i in 0..XMAS.len() {
+                                xmas_positions[y + i][x - i] = true;
+                            }
+                        }
+                    }
+
+                    if check_right {
+                        // Check down-right
+                        let mut check = true;
+                        for i in 0..XMAS.len() {
+                            if input[y + i][x + i] != XMAS[i] {
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if check {
+                            sum += 1;
+                            for i in 0..XMAS.len() {
+                                xmas_positions[y + i][x + i] = true;
+                            }
+                        }
+                    }
+                }
+
+                if check_up {
+                    // Check up
+                    let mut check = true;
+                    for i in 0..XMAS.len() {
+                        if input[y - i][x] != XMAS[i] {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if check {
+                        sum += 1;
+                        for i in 0..XMAS.len() {
+                            xmas_positions[y - i][x] = true;
+                        }
+                    }
+
+                    if check_left {
+                        // Check up-left
+                        let mut check = true;
+                        for i in 0..XMAS.len() {
+                            if input[y - i][x - i] != XMAS[i] {
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if check {
+                            sum += 1;
+                            for i in 0..XMAS.len() {
+                                xmas_positions[y - i][x - i] = true;
+                            }
+                        }
+                    }
+
+                    if check_right {
+                        // Check up-right
+                        let mut check = true;
+                        for i in 0..XMAS.len() {
+                            if input[y - i][x + i] != XMAS[i] {
+                                check = false;
+                                break;
+                            }
+                        }
+
+                        if check {
+                            sum += 1;
+                            for i in 0..XMAS.len() {
+                                xmas_positions[y - i][x + i] = true;
+                            }
+                        }
+                    }
+                }
+
+                if check_right {
+                    // Check right
+                    let mut check = true;
+                    for i in 0..XMAS.len() {
+                        if input[y][x + i] != XMAS[i] {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if check {
+                        sum += 1;
+                        for i in 0..XMAS.len() {
+                            xmas_positions[y][x + i] = true;
+                        }
+                    }
+                }
+
+                if check_left {
+                    // Check left
+                    let mut check = true;
+                    for i in 0..XMAS.len() {
+                        if input[y][x - i] != XMAS[i] {
+                            check = false;
+                            break;
+                        }
+                    }
+
+                    if check {
+                        sum += 1;
+                        for i in 0..XMAS.len() {
+                            xmas_positions[y][x - i] = true;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for (y, line) in xmas_positions.iter().enumerate() {
+        println!(
+            "{}",
+            line.iter()
+                .enumerate()
+                .map(|(x, c)| if *c { input[y][x] } else { '.' })
+                .collect::<String>()
+        );
+    }
+
+    println!("{}", sum);
 }
--- days/day4/src/part2.rs
+++ days/day4/src/part2.rs
@@ -1,6 +1,64 @@
+use itertools::Itertools;
 use utils::*;
 
 fn main() {
     let input = init!();
-    todo!("{}", input);
+
+    let mut sum = 0;
+
+    let input = input
+        .lines()
+        .map(|line| line.chars().collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+
+    let height = input.len();
+    let width = input[0].len();
+
+    let mut xmas_positions = input
+        .iter()
+        .map(|line| line.iter().map(|_| false).collect_vec())
+        .collect_vec();
+
+    for (y, line) in input.iter().enumerate() {
+        for (x, c) in line.iter().enumerate() {
+            if *c == 'A' {
+                let check_down = y + 1 < height;
+                let check_up = y >= 1;
+                let check_right = x + 1 < width;
+                let check_left = x >= 1;
+
+                if !check_up || !check_down || !check_left || !check_right {
+                    continue;
+                }
+
+                let top_left = input[y - 1][x - 1];
+                let top_right = input[y - 1][x + 1];
+                let bottom_left = input[y + 1][x - 1];
+                let bottom_right = input[y + 1][x + 1];
+
+                if matches!((top_left, bottom_right), ('M', 'S') | ('S', 'M'))
+                    && matches!((top_right, bottom_left), ('M', 'S') | ('S', 'M'))
+                {
+                    sum += 1;
+                    xmas_positions[y][x] = true;
+                    xmas_positions[y - 1][x - 1] = true;
+                    xmas_positions[y - 1][x + 1] = true;
+                    xmas_positions[y + 1][x - 1] = true;
+                    xmas_positions[y + 1][x + 1] = true;
+                }
+            }
+        }
+    }
+
+    for (y, line) in xmas_positions.iter().enumerate() {
+        println!(
+            "{}",
+            line.iter()
+                .enumerate()
+                .map(|(x, c)| if *c { input[y][x] } else { '.' })
+                .collect::<String>()
+        );
+    }
+
+    println!("{}", sum);
 }
